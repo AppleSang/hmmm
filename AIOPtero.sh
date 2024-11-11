52d7b07d684420aa5c96bd5a7c9925f8a7366df1
AppleSang <96937073+AppleSang@users.noreply.github.com>
2024-11-11 11:46:09 +0700

    Create AIOPtero.sh

---
+++ AIOPtero.sh
@@ -0,0 +1,19 @@
+cd /var/www/pterodactyl
+php artisan down
+curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+chmod -R 755 storage/* bootstrap/cache
+composer install --no-dev --optimize-autoloader
+php artisan view:clear
+php artisan config:clear
+php artisan migrate --seed --force
+chown -R www-data:www-data /var/www/pterodactyl/*
+php artisan queue:restart
+php artisan up
+yarn
+yarn add cross-env
+FOLDER="/var/www/pterodactyl"; WEBUSER="www-data"; USERSHELL="/bin/bash"; PERMISSIONS="www-data:www-data";
+sed -i -E -e "s|WEBUSER=\"www-data\" #;|WEBUSER=\"$WEBUSER\" #;|g" -e "s|USERSHELL=\"/bin/bash\" #;|USERSHELL=\"$USERSHELL\" #;|g" -e "s|OWNERSHIP=\"www-data:www-data\" #;|OWNERSHIP=\"$PERMISSIONS\" #;|g" $FOLDER/blueprint.sh
+blueprint -upgrade
+blueprint -i nebula.blueprint
+blueprint -i simplefavicons.blueprint
+blueprint -i simplefavicons.blueprint
