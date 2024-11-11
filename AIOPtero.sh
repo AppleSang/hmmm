0e8942e7b5244fc9937615d218859b0e71cccb91
AppleSang <96937073+AppleSang@users.noreply.github.com>
2024-11-11 12:06:01 +0700

    Update AIOPtero.sh

--- AIOPtero.sh
+++ AIOPtero.sh
@@ -14,6 +14,12 @@ yarn add cross-env
 FOLDER="/var/www/pterodactyl"; WEBUSER="www-data"; USERSHELL="/bin/bash"; PERMISSIONS="www-data:www-data";
 sed -i -E -e "s|WEBUSER=\"www-data\" #;|WEBUSER=\"$WEBUSER\" #;|g" -e "s|USERSHELL=\"/bin/bash\" #;|USERSHELL=\"$USERSHELL\" #;|g" -e "s|OWNERSHIP=\"www-data:www-data\" #;|OWNERSHIP=\"$PERMISSIONS\" #;|g" $FOLDER/blueprint.sh
 blueprint -upgrade
+y
+continue
+y
+y
 blueprint -i nebula.blueprint
+
+
 blueprint -i simplefavicons.blueprint
 blueprint -i simplefavicons.blueprint
